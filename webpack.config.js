6e99fbc19c051e5f91823f62a0740d6369efe9a4
Dalimil Hajek <dalimilhajek@gmail.com>
2017-01-20 15:37:26 +0000

    Fix webpack and bundling

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+	entry: ['./src/index.js'],
+	output: {
+		path: path.resolve(__dirname, './www'), // absolute path
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			// Webpack should run sources through Babel when it bundles them
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				query: {
+					presets: ["latest"]
+				}
+			}
+		]
+	}
+}
