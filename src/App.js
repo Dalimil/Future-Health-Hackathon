2edd0498e2ca3e953e804e787e1c9014bb198fd1
Dalimil Hajek <dalimilhajek@gmail.com>
2017-01-21 00:14:31 +0000

    Add private message view and intro view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 
 class App {
 
+    static get FIRST_VISIT_KEY() {
+        return "storage-first-visit";
+    }
+
     constructor() {
         console.log(Framework7);
 
         this.$$ = Dom7;
+        this.storage = window.localStorage;
         this.app = new Framework7({
             material: true
         });
         this.mainView = this.app.addView('.view-main');
-        
+
         document.addEventListener("deviceready", this.onDeviceReady);
 
-        $("#welcome-continue-btn").click(() => {
-            console.log("clicked");
+        this.checkFirstVisit();
+
+        $(".nav-messages").click(() => {
             this.mainView.router.loadPage('messages.html');
         });
+    }
 
-        // if new visit open ^^^^ TODO
+    checkFirstVisit() {
+        // comment out 'if' to debug
+        if (this.storage.getItem(App.FIRST_VISIT_KEY) == null) {
+            //$(".nav-forward").show();
+            this.mainView.router.loadPage('intro.html');
+            this.storage.setItem(App.FIRST_VISIT_KEY, "-");
+            /*$(".nav-forward").click(function() {
+                $(this).hide();
+            });*/
+        }
     }
     
     onDeviceReady() {
